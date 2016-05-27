1f61b6f55bc9366ec60b1e6733c281b64b8ea6ee
andy.keen <andy.keen@insidetrack.com>
2016-05-27 12:42:47 -0700

    initial commit

---
+++ handlers.js
@@ -0,0 +1,71 @@
+
+var config = require('./config'),
+    fs = require('fs'),
+    sys = require('sys');
+
+function home(response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end(fs.readFileSync('./static/index.html'));
+}
+
+function upload(response, postData) {
+    var files = JSON.parse(postData);
+    _upload(response, files.audio);  // writing audio file to disk
+    response.statusCode = 200;
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(files.audio.name);
+}
+
+function _upload(response, file) {
+    var fileRootName = file.name.split('.').shift(),
+        fileExtension = file.name.split('.').pop(),
+        filePathBase = config.upload_dir + '/',
+        fileRootNameWithBase = filePathBase + fileRootName,
+        filePath = fileRootNameWithBase + '.' + fileExtension,
+        fileID = 2,
+        fileBuffer;
+
+    while (fs.existsSync(filePath)) {
+        filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
+        fileID += 1;
+    }
+    file.contents = file.contents.split(',').pop();
+    fileBuffer = new Buffer(file.contents, "base64");
+    fs.writeFileSync(filePath, fileBuffer);
+}
+
+function serveStatic(response, pathname) {
+
+    var extension = pathname.split('.').pop(),
+        extensionTypes = {
+            'js': 'application/javascript',
+            'webm': 'video/webm',
+            'mp4': 'video/mp4',
+            'wav': 'audio/wav',
+            'ogg': 'audio/ogg',
+            'gif': 'image/gif'
+        };
+
+    response.writeHead(200, {
+        'Content-Type': extensionTypes[extension]
+    });
+    if (hasMediaType(extensionTypes[extension]))
+        response.end(fs.readFileSync('.' + pathname));
+    else
+        response.end(fs.readFileSync('./static' + pathname));
+}
+
+function hasMediaType(type) {
+    var isHasMediaType = false;
+    ['audio/wav', 'audio/ogg', 'video/webm', 'video/mp4'].forEach(function(t) {
+      if(t== type) isHasMediaType = true;
+    });
+    
+    return isHasMediaType;
+}
+
+exports.home = home;
+exports.upload = upload;
+exports.serveStatic = serveStatic;
